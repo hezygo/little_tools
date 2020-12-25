e2cc20f71a8a401c602b2b318849fb21b23c842a
HoLittlePig <58553737+hezygo@users.noreply.github.com>
2020-12-25 10:31:29 +0800

    Update and rename 解决SQLserver死锁问题.sql to resolve_the_SQLServer_deadlock_issue.sql
    修改名称和注释

---
+++ resolve_the_SQLServer_deadlock_issue.sql
@@ -0,0 +1,14 @@
+-- =============================================
+--文件    :createAndUpdate.sql
+--说明    :解决SQLserver死锁问题
+--时间    :2020/12/25 10:30:12
+--作者    :一直都没有暴富成功的小何
+--版本    :1.0
+-- =============================================
+-- 解决死锁问题
+select spid, blocked, loginame, last_batch, status, cmd, hostname, program_name
+from sysprocesses
+where spid in
+( select blocked from sysprocesses where blocked <> 0 ) or (blocked <>0)
+---进程号 直接替换spid
+kill spid
